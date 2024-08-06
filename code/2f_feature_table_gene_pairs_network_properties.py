392d73ebc30ce88d6453a5f12a6e85c0ea788619
ksegaba <keniaesegura@gmail.com>
2024-08-06 15:55:56 -0400

    Code to generate feature tables for network properties based on single genes and gene pair instances

---
+++ code/2f_feature_table_gene_pairs_network_properties.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python3
+# %%
+import json
+import pandas as pd
+import datatable as dt
+import numpy as np
+import networkx as nx
+
+__author__ = 'Kenia Segura Abá'
+
+def calculate_feature_values(df, gene_pairs, feature_type):
+    ''' Generate a feature table for a given set of instances (gene pairs) from 
+    the data in df, depending on the feature type (feature_type). Feature values 
+    are based on the 'Calculation for gene pair' column in the checklist file
+    
+    If the feature type is 'continuous' then feature values are calculated from 
+    an m x m adjacency matrix (df), where m is the  number of instances. Instance 
+    identifiers need to be set as the index and the column names of the dataframe.
+    
+    If the feature type is 'binary' then feature values are calculated from an 
+    m x n matrix (df) of categorical values. The instance identifiers should be 
+    set as the index of the dataframe and the columns are categorical variables.
+
+    Parameters:
+        - df (pd.DataFrame): data matrix to calculate feature values froms
+        - gene_pairs (pd.DataFrame): instance identifiers in two columns
+        - feature_type (str): type of feature to calculate feature values 
+                              for ('continuous' or 'binary')
+    
+    Returns a dataframe with the following columns:
+        If the feature_type is 'continuous':
+            - gene1 (str): gene 1 identifiers
+            - gene2 (str): gene 2 identifiers
+            - overlap (float): number of overlapping interactors between gene1 and gene2
+            - total (float): total number of unique interactors for gene1 and gene2
+            - percent overlap (float): percentage of overlapping interactors
+        
+        If the feature_type is 'binary':
+            - gene1 (str): gene 1 identifiers
+            - gene2 (str): gene 2 identifiers
+            - additional columns: binary values indicating whether gene1 and gene2
+                                  have the same value in the columns of df. The 
+                                  column names are the same as the column names 
+                                  in df.
+    '''
+    #
+    # Create feature values for continuous feature types
+    if feature_type == 'continuous':
+        G = nx.from_pandas_adjacency(df) # make graph
+        
+        features = []
+        for i in range(len(gene_pairs)):
+            gene1 = gene_pairs.gene1[i]
+            gene2 = gene_pairs.gene2[i]
+            
+            # Calculate feature values
+            try:
+                overlap = len(set(nx.common_neighbors(G, gene1, gene2)))
+                total = len(set(list(G.neighbors(gene1)) + list(G.neighbors(gene2))))
+                percent = (overlap/total) * 100
+                features.append([gene1, gene2, overlap, total, percent])
+            
+            except nx.exception.NetworkXError:
+                # At least one gene identifier is not in the network
+                features.append([gene1, gene2, np.nan, np.nan, np.nan])
+        
+        col_names=['gene1', 'gene2', '# overlapping', 'Total #', '% overlapping']
+        
+        return pd.DataFrame(features, columns=col_names)
+    
+    # Create feature values for binary feature types (same or not)
+    elif feature_type == 'binary':
+        features = []
+        for i in range(len(gene_pairs)):
+            gene1 = gene_pairs.gene1[i]
+            gene2 = gene_pairs.gene2[i]
+            
+            # Determine if the two genes have the same annotation/property
+            try:
+                features.append(df.loc[gene1].eq(df.loc[gene2]).astype('int'))
+        #
+            except KeyError:
+                # At least one gene identifier is not in the network
+                features.append(pd.Series(np.nan, index=np.arange(len(df.columns))))
+        #
+        return pd.concat([gene_pairs, pd.concat(features, axis=1).T], axis=1)
+    #
+    else:
+        raise ValueError('Feature type must be either "continuous" or "binary".')
+
+
+def apply_transformations(df):
+    ''' Apply transformations to a dataframe of continuous feature values.
+    
+    Parameters:
+        - df (pd.DataFrame): A data frame of feature values
+    
+    Returns an expanded dataframe with additional transformations on continuous columns:
+        - Binned values
+        - Logarithm
+        - Reciprocal
+        - Squared
+    '''
+    df_list = []
+    #
+    # Apply the transformations to individual columns
+    for column in df.columns:
+        if column in ['# overlapping', '% overlapping', 'Total #']:
+            # Bin the feature value
+            bins = pd.qcut(df[column], q=4, labels=False, duplicates='drop')
+            bins.name = column + '_bin'
+            
+            # Calculate the log
+            log_vals = df[column].apply(lambda x: np.log10(x) if x > 0 else np.nan)
+            log_vals.name = column + '_log'
+            
+            # Calculate the reciprocal
+            reciprocal = df[column].apply(lambda x: 1/x if x != 0 else np.nan)
+            reciprocal.name = column + '_reciprocal'
+            
+            # Calculate the square
+            square = df[column] ** 2
+            square.name = column + '_squared'
+            
+            df_list.append(pd.concat([df[column], bins, log_vals, reciprocal,
+                                      square], axis=1))
+            
+        else:
+            # No transformations will be applied to binary features
+            df_list.append(df[column])
+    
+    return pd.concat(df_list, axis=1)
+
+
+def assign_feature_column_names(df, feature_name):
+    ''' Assign feature column names to the columns of a dataframe according to 
+    the feature engineering checklist file.
+    
+    Parameters:
+        - df (pd.DataFrame): dataframe of feature values
+        - feature_name (str): name of the feature category in the 'Feature name'
+                              column of the checklist file
+    
+    Returns: 
+        - col_names (dict): a dictionary containing the original column names 
+                            as keys and the new column names as values.
+        - col_to_drop (list): a list of column names to drop from the dataframe
+    '''
+    col_names = {} # column name mapping
+    col_to_drop = [] # columns to drop
+    subset = checklist[checklist['NEW Feature name'] == feature_name]
+    #
+    if feature_name in ['aranet', 'ppi']:
+        # Determine the new column names for df
+        for col in df.columns:
+            if col.split('_')[0] in subset['Calculation for gene pair'].unique():
+                if col.endswith('_bin'):
+                    new_col = subset.loc[(subset['NEW Feature name'] == feature_name) &
+                        (subset['Calculation for gene pair'] == col.split('_')[0])
+                        & (subset['Transformation'] == 'Binned'),
+                        'ML model feature name'].values[0]
+                
+                elif col.endswith('_log'):
+                    new_col = subset.loc[(subset['NEW Feature name'] == feature_name) &
+                        (subset['Calculation for gene pair'] == col.split('_')[0])
+                        & (subset['Transformation'] == 'Log'),
+                        'ML model feature name'].values[0]
+                
+                elif col.endswith('_reciprocal'):
+                    new_col = subset.loc[(subset['NEW Feature name'] == feature_name) &
+                        (subset['Calculation for gene pair'] == col.split('_')[0])
+                        & (subset['Transformation'] == 'Reciprocal'),
+                        'ML model feature name'].values[0]
+                
+                elif col.endswith('_squared'):
+                    new_col = subset.loc[(subset['NEW Feature name'] == feature_name) &
+                        (subset['Calculation for gene pair'] == col.split('_')[0])
+                        & (subset['Transformation'] == 'Squared'),
+                        'ML model feature name'].values[0]
+                
+                else:
+                    new_col = subset.loc[(subset['NEW Feature name'] == feature_name) &
+                        (subset['Calculation for gene pair'] == col)
+                        & (subset['Transformation'].isna()),
+                        'ML model feature name'].values[0]
+                #
+                col_names[col] = new_col
+                #
+            else:
+                col_names[col] = col
+        
+    if feature_name == 'all clust':
+        for col in df.columns:
+            try:
+                new_col = subset['ML model feature name'].\
+                    loc[subset['ML model feature name'].str.contains(col)].values[0]
+                col_names[col] = new_col
+            #
+            except IndexError:
+                if col in ['gene1', 'gene2']:
+                    col_names[col] = col
+                else:
+                    col_to_drop.append(col)
+    #
+    return col_names, col_to_drop
+
+
+# %%
+# Feature checklist file
+checklist = pd.read_csv('../data/2021_cusack_data/06_network_properties_feature_list.csv')
+
+# Instances file
+gene_pairs = pd.read_csv('/home/seguraab/ara-kinase-prediction/data/instances_dataset_1.txt', sep='\t')
+
+# %%
+# Create the network properties feature table
+for feature_name in checklist['NEW Feature name'].unique():
+    if feature_name == 'aranet':
+        print("Creating the AraNet features...")
+        adj_mat = dt.fread('../data/Features/network_properties_single_genes_aranet_adjacency_matrix.csv.gz').to_pandas()
+        adj_mat.set_index('gene', inplace=True)
+        #
+        aranet_features = calculate_feature_values(adj_mat, gene_pairs, 'continuous') # calculate continuous feature values
+        aranet_features = apply_transformations(aranet_features) # apply transformations
+        col_map, col_to_drop = assign_feature_column_names(aranet_features, 'aranet') # get column names from the checklist file
+        aranet_features.rename(columns=col_map, inplace=True) # rename the columns according to mapping
+        #
+        if len(col_to_drop) > 0: # drop columns that are not in the checklist file
+            aranet_features.drop(columns=col_to_drop, inplace=True)
+        #
+        del adj_mat, col_map, col_to_drop
+    #
+    if feature_name == 'all clust':
+        print("Creating the co-expression features...")
+        co_expr = dt.fread('../data/Features/network_properties_single_genes_co_expression_clusters.csv').to_pandas()
+        co_expr.set_index('gene', inplace=True)
+        #
+        co_expr_features = calculate_feature_values(co_expr, gene_pairs, 'binary') # calculate binary feature values
+        col_map, col_to_drop = assign_feature_column_names(co_expr_features, 'all clust') # get column names from the checklist file
+        co_expr_features.rename(columns=col_map, inplace=True) # rename the columns according to mapping
+        #
+        if len(col_to_drop) > 0: # drop columns that are not in the checklist file
+            co_expr_features.drop(columns=col_to_drop, inplace=True)
+        #
+        del co_expr, col_map, col_to_drop
+    #
+    if feature_name == 'ppi':
+        print("Creating the PPI features...")
+        adj_mat = dt.fread('../data/Features/network_properties_single_genes_ppi_adjacency_matrix.csv.gz').to_pandas()
+        adj_mat.set_index('gene', inplace=True)
+        #
+        ppi_features = calculate_feature_values(adj_mat, gene_pairs, 'continuous') # calculate continuous feature values
+        ppi_features = apply_transformations(ppi_features) # apply transformations
+        col_map, col_to_drop = assign_feature_column_names(ppi_features, 'ppi') # get column names from the checklist file
+        ppi_features.rename(columns=col_map, inplace=True) # rename the columns according to mapping
+        #
+        if len(col_to_drop) > 0: # drop columns that are not in the checklist file
+            ppi_features.drop(columns=col_to_drop, inplace=True)
+        #
+        del adj_mat, col_map
+
+# %%
+# Ensure the data frames have the gene pairs in the same order
+if (aranet_features.gene1.equals(co_expr_features.gene1)) &\
+    (aranet_features.gene1.equals(ppi_features.gene1)) &\
+    (aranet_features.gene2.equals(co_expr_features.gene2)) &\
+    (aranet_features.gene2.equals(ppi_features.gene2)):
+    
+    feat_table = pd.concat([aranet_features, co_expr_features.iloc[:, 2:],
+                        ppi_features.iloc[:, 2:]], axis=1)
+    
+    print("Saving the feature table to ../data/Features/network_properties_gene_pairs_features.csv")
+    feat_table.to_csv('../data/Features/network_properties_gene_pairs_features.csv', index=False)
+    
+    print("Feature table dimensions:", feat_table.shape)
+    print("Checklist file dimensions:", checklist.shape)
+
+else:
+    print("Gene pairs are not in the same order. Please check the dataframes.")
---
+++ code/2f_feature_table_single_genes_network_properties.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# %%
+import json
+import pandas as pd
+import numpy as np
+import networkx as nx
+
+__author__ = 'Kenia Segura Abá'
+
+# Feature checklist file
+checklist = pd.read_csv('../data/2021_cusack_data/06_network_properties_feature_list.csv')
+
+# Open the JSON files in the checklist
+aranet = json.load(open(checklist['File path'].unique()[0])) # AraNet genetic interactions
+co_expr = json.load(open(checklist['File path'].unique()[1])) # Co-expression network
+ppi = json.load(open(checklist['File path'].unique()[2])) # Protein-protein interactions
+
+# Instances file
+genes = pd.read_csv('/home/seguraab/ara-kinase-prediction/data/instances_dataset_singles.txt')
+
+# %%
+########################## Create the AraNet features ##########################
+# The number of interactions as a feature
+genes.insert(1, 'NumInteractions', genes.apply(lambda x: len(aranet[x['gene']]) \
+             if x['gene'] in aranet.keys() else np.nan, axis=1))
+
+# Adjacency matrix features
+G = nx.Graph()
+for key, values in aranet.items():
+    for value in values:
+        G.add_edge(key, value)
+
+adj_mat = nx.to_pandas_adjacency(G)
+adj_mat.index.name = 'gene'
+# adj_mat.to_csv('../data/Features/network_properties_single_genes_aranet_adjacency_matrix.csv.gz', compression='gzip')
+
+# check to make sure the number of edges add up to the number of interactions (NumInteractions)
+adj_mat.sum()
+# adj_mat.iloc[0,:].loc[adj_mat.iloc[0,:]==1].index
+# aranet['AT1G06190']
+'''Note: The reason why adj_mat.sum() does not match NumInteractions in genes, 
+is because the dictionary values are missing some of the interacting gene pairs.
+For example, aranet['AT1G06190'] is missing "AT1G04870" and "AT1G02150" but in
+the values, but aranet['AT1G04870'] and aranet['AT1G02150'] have "AT1G06190" in 
+their values. Thus, I will use networkx to calculate the number of interactions 
+for each node (i.e. gene).'''
+
+# Calculate the number of interactions for each node (gene)
+num_int = pd.DataFrame.from_dict(dict(G.degree()), orient='index')
+num_int.columns = ['NumInteractions']
+num_int.index.name = 'gene'
+# num_int.to_csv('../data/Features/network_properties_single_genes_aranet_num_interactions.csv')
+genes.drop(columns='NumInteractions', inplace=True)
+
+del G, adj_mat, num_int
+
+# %%
+################### Create the co-expression network features ##################
+# Convert the JSON into a dataframe and sort the index
+
+# The feature values are the assigned co-expression cluster numbers for each gene
+co_expr_feat = pd.DataFrame.from_dict(co_expr, orient='index').sort_index().T
+co_expr_feat.index.name = 'gene'
+co_expr_feat.to_csv('../data/Features/network_properties_single_genes_co_expression_clusters.csv')
+
+# One-hot encode the co-expression cluster numbers
+encoded_co_expr = {}
+for col in co_expr_feat.columns:
+    encoded_co_expr[col] = pd.get_dummies(co_expr_feat[col], prefix=f'{col}_cluster')
+
+pd.concat(encoded_co_expr.values(), ignore_index=False, axis=1).astype('int').to_csv(
+    '../data/Features/network_properties_single_genes_co_expression_clusters_onehot.csv.gz',
+    compression='gzip', chunksize=1000)
+
+del co_expr_feat, encoded_co_expr
+
+# %%
+################ Create the protein-protein interaction features ###############
+# Adjacency matrix features
+G = nx.Graph()
+for key, values in ppi.items():
+    for value in values:
+        G.add_edge(key, value)
+
+adj_mat = nx.to_pandas_adjacency(G)
+adj_mat.index.name = 'gene'
+# adj_mat.to_csv('../data/Features/network_properties_single_genes_ppi_adjacency_matrix.csv.gz', compression='gzip')
+
+# check to make sure the number of edges add up to the number of interactions (NumInteractions)
+genes.insert(1, 'NumInteractions', genes.apply(lambda x: len(ppi[x['gene']]) \
+             if x['gene'] in ppi.keys() else np.nan, axis=1))
+
+adj_mat.sum()
+# adj_mat.iloc[0,:].loc[adj_mat.iloc[0,:]==1].index
+# ppi['AT4G02110']
+# ppi['AT4G16760']
+'''Note: The same thing as with AraNet is occurring with the PPI data.'''
+
+# Calculate the number of interactions for each node (gene)
+num_int = pd.DataFrame.from_dict(dict(G.degree()), orient='index')
+num_int.columns = ['NumInteractions']
+num_int.index.name = 'gene'
+# num_int.to_csv('../data/Features/network_properties_single_genes_ppi_num_interactions.csv')
+genes.drop(columns='NumInteractions', inplace=True)
