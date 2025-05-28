f8c93047c1d37296b9e9c9825778d6de34fff1e4
ksegaba <keniaesegura@gmail.com>
2025-05-28 17:44:12 -0400

    Add -y_path argument and include label(s) for loop in slurm  script

--- code/4_feature_selection.py
+++ code/4_feature_selection.py
@@ -6,11 +6,12 @@ This script also generates a SLURM sbatch file to run the Random Forest models
 using the generated feature sets.
 
 Arguments:
-    --model_path: Path to the pre-trained Random Forest model.
-    --data_path: Path to the dataset CSV file.
-    --output_dir: Directory to save the generated feature sets.
-    --sbatch_name: Name to save the generated SLURM sbatch file.
-    --tag: Prefix for the RF model output files.
+    -model_path: Path to the pre-trained Random Forest model.
+    -X_path: Path to the feature table CSV file.
+    -y_path: Path to the labels CSV file (optional).
+    -output_dir: Directory to save the generated feature sets.
+    -sbatch_name: Name to save the generated SLURM sbatch file.
+    -tag: Prefix for the RF model output files.
 
 Dependencies:
     - Conda environment: conda activate py310
@@ -76,10 +77,17 @@ def generate_feature_sets(shap_values, output_dir):
     return feature_sets
 
 
-def generate_sbatch_file(output_dir, data_path, sbatch_name, tag, feature_sets):
+def generate_sbatch_file(output_dir, X_path, y_path, sbatch_name, tag, feature_sets):
     '''4) Generate a SLURM sbatch file to run RF models with different feature
     sets on the HPC cluster.'''
     #
+    # Get the list of label(s) from the y_path file if not ""
+    if y_path != "":
+        labels = pd.read_csv(y_path)
+        label_list = [
+            col for col in labels.columns if col not in ['ID', 'Set', 'dataset']]
+    #
+    # Now generate the sbatch file
     with open(os.path.join(output_dir, sbatch_name), 'w') as f:
         f.write("#!/bin/bash --login\n")
         f.write("#SBATCH --job-name=rf_feature_selection\n")
@@ -96,16 +104,24 @@ def generate_sbatch_file(output_dir, data_path, sbatch_name, tag, feature_sets):
         f.write(f"cd {output_dir}\n")
         f.write(
             f"FEATURE_SETS=($(ls feature_set_above_p*.txt))\n\n")
+        if y_path != "":  # loop through each label
+            f.write(f"LABELS=({" ".join(label_list)})\n\n")
+            f.write("for label in ${LABELS[@]}; do\n")
         f.write(
             "python /mnt/home/seguraab/Shiu_Lab/ML-Pipeline/ML_classification.py \\\n")
-        f.write(f"\t-df {data_path} -sep ,\\\n")
-        f.write("\t-y_name Class -pos positive -cl_train positive,negative \\\n")
+        f.write(f"\t-df {X_path} -sep ,\\\n")
+        if y_path == "":  # label is in the X_path file and called "Class"
+            f.write("\t-y_name Class -pos positive -cl_train positive,negative \\\n")
+        else:  # label(s) are in a separate file
+            f.write("\t-y_name ${label}\\\n")
         f.write("\t-feat ${FEATURE_SETS[${SLURM_ARRAY_TASK_ID}]} \\\n")
         f.write(
             "\t-alg RF -n_jobs 12 -b 100 -cv_num 10 -x_norm t -gs_reps 10 -cm t \\\n")
         f.write(f"\t-save {tag}")
         f.write("_${FEATURE_SETS[${SLURM_ARRAY_TASK_ID}]} \\\n")
         f.write("\t-plots f\n\n")
+        if y_path != "":
+            f.write("done\n\n")
         f.write("conda deactivate\n\nscontrol show job $SLURM_JOB_ID\n")
 
 
@@ -114,39 +130,46 @@ if __name__ == "__main__":
     # Argument parser
     parser = argparse.ArgumentParser(
         description="Feature selection using SHAP values and SLURM job generation.")
-    parser.add_argument("--model_path", type=str, required=True,
+    parser.add_argument("-model_path", type=str, required=True,
                         help="Path to the pre-trained Random Forest model.")
-    parser.add_argument("--data_path", type=str, required=True,
-                        help="Path to the dataset CSV file.")
-    parser.add_argument("--output_dir", type=str, required=True,
+    parser.add_argument("-X_path", type=str, required=True,
+                        help="Path to the feature table CSV file.")
+    parser.add_argument("-y_path", type=str, required=False,
+                        help="Path to the labels CSV file", default="")
+    parser.add_argument("-output_dir", type=str, required=True,
                         help="Directory to save the generated feature sets and SLURM script.")
-    parser.add_argument("--sbatch_name", type=str, required=True,
+    parser.add_argument("-sbatch_name", type=str, required=True,
                         help="Name to save as the generated SLURM sbatch file.")
-    parser.add_argument("--tag", type=str, default='',
+    parser.add_argument("-tag", type=str, default='',
                         help="Prefix for the RF model output files.")
 
     args = parser.parse_args()
 
     # Paths
     model_path = args.model_path
-    data_path = args.data_path
+    X_path = args.X_path
+    y_path = args.y_path
     output_dir = args.output_dir
     sbatch_name = args.sbatch_name
     tag = args.tag
 
     # Arguments for example usage
     # model_path = "/home/seguraab/ara-kinase-prediction/data/2021_cusack_data/Dataset_4_Features/output_clf/RF/rf_clf_imputed_Dataset_4by_bin_cat_cont_type_models.pkl"
-    # data_path = "/home/seguraab/ara-kinase-prediction/data/2021_cusack_data/Dataset_4_Features/Imputed_Dataset_4_X_train_final_table_by_bin_cat_cont_type.csv"
+    # X_path = "/home/seguraab/ara-kinase-prediction/data/2021_cusack_data/Dataset_4_Features/Imputed_Dataset_4_X_train_final_table_by_bin_cat_cont_type.csv"
+    # y_path = ""
     # output_dir = "/home/seguraab/ara-kinase-prediction/data/2021_cusack_data/Dataset_4_Features/output_clf/RF_FS"
     # sbatch_name = "run_rf_feature_selection_models.sb"
     # tag = "rf_fs_clf_imputed_Dataset_4by_bin_cat_cont_type"
 
     # Load data and model
-    data = dt.fread(data_path).to_pandas()
+    data = dt.fread(X_path).to_pandas()
     data = data.set_index(data.columns[0])
 
     # Replace "target" with your target column name
-    X = data.drop(columns=["Class"])
+    if "Class" in data.columns:
+        X = data.drop(columns=["Class"])
+    else:
+        raise ValueError("The 'Class' column is not found in the data.")
     del data
 
     # Load the pre-trained Random Forest model
@@ -159,4 +182,5 @@ if __name__ == "__main__":
     feature_sets = generate_feature_sets(shap_values, output_dir)
 
     # Generate SLURM sbatch file
-    generate_sbatch_file(output_dir, data_path, sbatch_name, tag, feature_sets)
+    generate_sbatch_file(output_dir, X_path, y_path,
+                         sbatch_name, tag, feature_sets)
